de792c7e84ae2d12bda7c3f7fa9489ac7eb68b37
Daniel Rocha <rochadaniel@acad.ifma.edu.br>
2023-09-26 14:25:48 +0000

    added: git init

---
+++ src/main/java/com/github/edulook/look/LookApplication.java
@@ -0,0 +1,13 @@
+package com.github.edulook.look;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LookApplication.class, args);
+	}
+
+}
