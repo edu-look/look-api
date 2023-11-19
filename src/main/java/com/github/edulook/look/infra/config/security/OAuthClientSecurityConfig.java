c77670ee7aaf956ff749a9201c6b0c20d3e42b00
Daniel Rocha <rochadaniel@acad.ifma.edu.br>
2023-11-19 06:13:41 +0000

    refac: removido código morto

--- src/main/java/com/github/edulook/look/infra/config/security/OAuthClientSecurityConfig.java
+++ src/main/java/com/github/edulook/look/infra/config/security/OAuthClientSecurityConfig.java
@@ -7,8 +7,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
-import com.google.common.annotations.Beta;
-
 
 @Configuration
 @EnableWebSecurity
@@ -21,38 +19,6 @@ public class OAuthClientSecurityConfig {
                 auth.anyRequest().authenticated();
             })
             .oauth2Login(Customizer.withDefaults())
-            .build();
-    }
-
-    // @Bean
-    @Beta
-    @Deprecated
-    SecurityFilterChain securityFilterChainWithRedirectionEndpoint(HttpSecurity http) throws Exception {
-        var ROOT = "/";
-        return http
-            .authorizeHttpRequests( auth -> {
-                auth.anyRequest().authenticated();
-            })
-            .oauth2Login(oauth2Login -> oauth2Login
-                .redirectionEndpoint(redirectionEndpoint ->
-                    redirectionEndpoint
-                        .baseUri(ROOT))
-                .defaultSuccessUrl(ROOT, Boolean.TRUE)
-            )
-            .build();
-    }
-
-    // @Bean
-    @Beta
-    @Deprecated
-    public SecurityFilterChain securityFilterChainWithJWT(HttpSecurity http) throws Exception {
-        // https://github.com/Baeldung/spring-security-oauth/tree/master/oauth-jwt
-
-        return http
-            .authorizeHttpRequests( auth -> { 
-                auth.anyRequest().authenticated();
-             })
-            .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
             .build();
     }
 }
