e46e5124205889864eca2e4482b2cd5dfa58be52
Ricardo Lemos <rycardokk@gmail.com>
2023-12-27 17:12:16 -0300

    feat: Implementacao para salvar o course

--- src/main/java/com/github/edulook/look/infra/worker/CourseWorker.java
+++ src/main/java/com/github/edulook/look/infra/worker/CourseWorker.java
@@ -1,55 +1,37 @@
 package com.github.edulook.look.infra.worker;
 
 
-import com.github.edulook.look.core.model.Course;
 import com.github.edulook.look.core.repository.CourseRepository;
-import com.github.edulook.look.endpoint.io.course.CourseDTO;
 import com.github.edulook.look.infra.worker.events.course.AnnouncementEvent;
+import com.github.edulook.look.infra.worker.events.course.CourseEvent;
 import com.github.edulook.look.infra.worker.events.course.WorkMaterialEvent;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.EventListener;
-import org.springframework.stereotype.Component;
+
 
 @Slf4j
 @Configuration
 public class CourseWorker {
 
-    private final FirebaseApp firebaseApp;
+    private final CourseRepository courseRepository;
 
-    public CourseWorker (FirebaseApp firebaseApp){
-        this.firebaseApp = firebaseApp;
+    public CourseWorker(CourseRepository courseRepository) {
+        this.courseRepository = courseRepository;
     }
 
     @EventListener
     public void processorWorkMaterial(WorkMaterialEvent course) {
-        log.info("saving work materialmaterial {}", course.getCourseId());
-        DatabaseReference workMaterialRef = FirebaseDatabase.getInstance(firebaseApp).getReference("workMaterials");
-        String workMaterialKey = workMaterialRef.push().getKey();
-        Course courseMaterial = new Course(
-                course.getCourseId(),
-                course.getId(),
-                course.getDescription(),
-                course.getTitle(),
-                course.getCreatedAt()
-        );
-
-        // Salvando os dados no banco de dados
-        workMaterialRef.child(workMaterialKey).setValue(courseMaterial, (databaseError, databaseReference) -> {
-            if (databaseError != null) {
-                log.error("Data could not be saved. " + databaseError.getMessage());
-            } else {
-                log.info("Work material saved with key: {}", workMaterialKey);
-            }
-        });
     }
 
     @EventListener
     public void processorAnnouncement(AnnouncementEvent announcement) {
         log.info("saving announcement {}", announcement.getCourseId());
+    }
+
+    @EventListener
+    public void eventCourse(CourseEvent courseEvent) {
+        courseRepository.save(courseEvent.getCourse());
+
     }
 }
