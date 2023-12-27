d0279ed75bd1595c273cd80fbd7f6f4f6c813543
Ricardo Lemos <rycardokk@gmail.com>
2023-12-27 16:52:29 -0300

    fix: Correcao de pacotes

--- src/main/java/com/github/edulook/look/infra/worker/events/course/WorkMaterialEvent.java
+++ src/main/java/com/github/edulook/look/infra/worker/events/course/WorkMaterialEvent.java
@@ -2,10 +2,11 @@ package com.github.edulook.look.infra.worker.events.course;
 
 import lombok.Builder;
 import lombok.Getter;
-import org.springframework.context.ApplicationEvent;
 
-import java.time.Clock;
+
+
 import java.util.List;
+
 
 import static com.github.edulook.look.core.model.Course.WorkMaterial;
 import static com.github.edulook.look.core.model.Course.WorkMaterial.Material;
