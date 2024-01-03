db51e0669e3a17a33886bc5009018b332abacbd7
Daniel Rocha <rochadaniel@acad.ifma.edu.br>
2024-01-03 20:36:31 -0300

    docs: Adicionado documentação da API (OpenAPI)

--- src/main/java/com/github/edulook/look/endpoint/io/course/MaterialDTO.java
+++ src/main/java/com/github/edulook/look/endpoint/io/course/MaterialDTO.java
@@ -4,27 +4,21 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.github.edulook.look.core.data.PageContent;
 import com.github.edulook.look.core.data.Range;
 import lombok.Builder;
-import lombok.Getter;
 
-import javax.validation.constraints.NotBlank;
 import java.util.List;
 import java.util.Optional;
 
 @Builder
 public record MaterialDTO(
-    @NotBlank
+//    @NotBlank
     String description,
     List<ContentMaterialDTO> materials
 ){
     @Builder
     @JsonInclude(JsonInclude.Include.NON_EMPTY)
     public record ContentMaterialDTO(
-        @NotBlank
         String id,
-        @NotBlank
         String name,
-
-        @NotBlank
         String description,
 
         String origin,
