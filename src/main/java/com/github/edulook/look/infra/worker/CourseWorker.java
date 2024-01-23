c852fee5feb8e76355e2a649bb465a53fa07f8ac
Daniel Rocha <rochadaniel@acad.ifma.edu.br>
2024-01-22 23:57:35 -0300

    refac: Removido as configurações do firebase

--- src/main/java/com/github/edulook/look/infra/worker/CourseWorker.java
+++ src/main/java/com/github/edulook/look/infra/worker/CourseWorker.java
@@ -1,27 +1,18 @@
 package com.github.edulook.look.infra.worker;
 
-import com.github.edulook.look.core.model.Course;
-import com.github.edulook.look.core.repository.CourseRepository;
-import com.github.edulook.look.endpoint.io.course.CourseDTO;
 import com.github.edulook.look.infra.worker.events.course.AnnouncementEvent;
 import com.github.edulook.look.infra.worker.events.course.CheckMaterialLinkEditEvent;
 import com.github.edulook.look.infra.worker.events.course.WorkMaterialEvent;
 import com.google.api.services.classroom.Classroom;
 import com.google.api.services.classroom.model.CourseWorkMaterial;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.stereotype.Component;
 
 import java.io.IOException;
-import java.sql.SQLOutput;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -31,40 +22,21 @@ import java.util.regex.Pattern;
 public class CourseWorker {
     @Value("${server.address}")
     private String address;
-    private final FirebaseApp firebaseApp;
     private final Classroom classroom;
 
-
-    public CourseWorker(FirebaseApp firebaseApp, Classroom classroom) {
-        this.firebaseApp = firebaseApp;
+    public CourseWorker(Classroom classroom) {
         this.classroom = classroom;
     }
 
     @EventListener
     public void processorWorkMaterial(WorkMaterialEvent course) {
+        // TODO
         log.info("saving work materialmaterial {}", course.getCourseId());
-        DatabaseReference workMaterialRef = FirebaseDatabase.getInstance(firebaseApp).getReference("workMaterials");
-        String workMaterialKey = workMaterialRef.push().getKey();
-        Course courseMaterial = new Course(
-                course.getCourseId(),
-                course.getId(),
-                course.getDescription(),
-                course.getTitle(),
-                course.getCreatedAt()
-        );
-
-        // Salvando os dados no banco de dados
-        workMaterialRef.child(workMaterialKey).setValue(courseMaterial, (databaseError, databaseReference) -> {
-            if (databaseError != null) {
-                log.error("Data could not be saved. " + databaseError.getMessage());
-            } else {
-                log.info("Work material saved with key: {}", workMaterialKey);
-            }
-        });
     }
 
     @EventListener
     public void processorAnnouncement(AnnouncementEvent announcement) {
+        // TODO
         log.info("saving announcement {}", announcement.getCourseId());
     }
 
