732d20b1e5708f37dd38f31f4ac0b3220301e9e1
Ricardo Lemos <rycardokk@gmail.com>
2024-01-30 17:00:09 -0300

    feat: implementacao JPA

--- src/main/java/com/github/edulook/look/core/model/Course.java
+++ src/main/java/com/github/edulook/look/core/model/Course.java
@@ -4,6 +4,7 @@ package com.github.edulook.look.core.model;
 import com.github.edulook.look.core.data.Option;
 import com.github.edulook.look.core.data.PageContent;
 import com.github.edulook.look.core.data.Range;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -13,20 +14,31 @@ import lombok.Setter;
 import java.util.List;
 import java.util.Optional;
 
+@Entity
+@Table(name = "courses")
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class Course { 
+public class Course {
+    @Id
     private String id;
+    @Column
     private String state;
+    @Column
     private String description;
+    @Column
     private String name;
+    @Column(name = "owner_id")
     private String ownerId;
+    @Column
     private String room;
+    @Column
     private String section;
+    @Column
     private String updated;
+    @OneToMany(mappedBy = "course", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Teacher> teachers;
 
     public Course(String courseId, String id, String description, String title, String createdAt) {}
@@ -38,44 +50,88 @@ public class Course {
         return !isValid();
     }
 
+    @Entity
+    @Table(name = "announcements")
     @Getter
     @Setter
     @AllArgsConstructor
     @NoArgsConstructor
     @Builder
     public static class Announcement {
+        @Id
         private String id;
+        @Column(name = "course_id")
         private String courseId;
+        @Column
         private String content;
+        @Column
         private String createdAt;
+        @Column
         private String owner;
     }
 
+    @Entity
+    @Table(name = "work_materials")
     @Getter
     @Setter
     @AllArgsConstructor
     @NoArgsConstructor
     @Builder
     public static class WorkMaterial {
+        @Id
         private String id;
+        @Column(name = "course_id")
         private String courseId;
+        @Column
         private String title;
+        @Column
         private String description;
+        @Column
         private String createdAt;
+        @OneToMany(mappedBy = "workMaterial", cascade = CascadeType.ALL, orphanRemoval = true)
         private List<Material> materials;
 
+        @Entity
+        @Table(name = "material")
         @Builder
         @Getter
         @Setter
+        @AllArgsConstructor
+        @NoArgsConstructor
         public static class Material {
+            @Id
             private String id;
+            @Column
             private String name;
+            @Column
             private String description;
+            @Column
             private String type;
+            @Column
             private String originLink;
+            @Column
             private String previewLink;
-            private Optional<PageContent> content;
-            private Optional<Option> option;
+
+            private PageContent content;
+
+            private Option option;
+
+            public Optional<PageContent> getContent() {
+                return Optional.ofNullable(content);
+            }
+
+            public void setContent(Optional<PageContent> content) {
+                this.content = content.orElse(null);
+            }
+
+            public Optional<Option> getOption() {
+                return Optional.ofNullable(option);
+            }
+
+            public void setOption(Optional<Option> option) {
+                this.option = option.orElse(null);
+            }
+
         }
     }
 }
--- src/main/java/com/github/edulook/look/core/model/Teacher.java
+++ src/main/java/com/github/edulook/look/core/model/Teacher.java
@@ -1,19 +1,29 @@
 package com.github.edulook.look.core.model;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Entity
+@Table(name = "teachers")
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class Teacher {
+    @Id
     private String id;
+    @Column
     private String email;
+    @Column
     private String photo;
-    private String name;   
+    @Column
+    private String name;
+    @ManyToOne
+    @JoinColumn(name = "course_id")
+    private Course course;
 }
