7b9f6b37e679cefcca61e9a0b0dfa0e8256c07be
Ricardo Lemos <rycardokk@gmail.com>
2024-02-01 21:28:40 -0300

    refac: criacao do model específico

---
+++ src/main/java/com/github/edulook/look/core/model/PageContent.java
@@ -0,0 +1,36 @@
+package com.github.edulook.look.core.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+@Entity
+@Builder
+@Data
+@NoArgsConstructor
+public class PageContent {
+    @Id
+    private Long id;
+    private Integer size;
+
+    @OneToMany(mappedBy = "pageContent")
+    private List<Page> pages;
+
+    public static PageContent fromCollection(List<Page> pages) {
+        int size = pages.size();
+        return new PageContent(null, size, pages);
+    }
+
+    private PageContent(Long id, Integer size, List<Page> pages) {
+        this.id = id;
+        this.size = size;
+        this.pages = pages;
+    }
+
+    public static Optional<PageContent> None() {
+        return Optional.empty();
+    }
+}
